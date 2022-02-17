71810604b30d519da0d8d3a7630c887939cf407d
John <johnmulvey48@yahoo.com>
2022-02-17 10:04:33 -0500

    git@github.com:Jmulvez/phase-1-constructor-functions-lab.git

---
+++ index.js
@@ -0,0 +1,22 @@
+class scooter {
+    constructor(year, color, model) {
+        this.year = year;
+        this.color = color;
+        this.model = model;
+    }
+}
+
+class driver {
+    constructor(name, age, experience) {
+        this.name = name;
+        this.age = age;
+        this.experience = experience;
+    }
+}
+
+class pickupLocation {
+    constructor(address, city) {
+        this.address = address;
+        this.city = city;
+    }
+}
